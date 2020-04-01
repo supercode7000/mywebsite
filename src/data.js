8c04614f81b04ec39e3a2058209f1923c64a6afb
supercode7000 <anass@super-code.de>
2020-04-01 13:28:44 +0200

    data export

---
+++ src/data.js
@@ -0,0 +1,8 @@
+export const SuperHeroItems = [
+    { urlImage: 'https://comicvine1.cbsistatic.com/uploads/scale_small/12/124259/7254202-wolv2020001_silva.jpg', id: 1, name: 'Wolverine', email: 'wolwerine@super.de' },
+    { urlImage: 'https://comicvine1.cbsistatic.com/uploads/scale_small/10/104700/4831078-spider-man%201.jpg', id: 2, name: 'Spider-Man', email: 'spiderman@super.de' },
+    { urlImage: 'https://comicvine1.cbsistatic.com/uploads/scale_small/11139/111399535/7231595-thor2020005-cvr.jpg', id: 3, name: 'Thor', email: 'thor@super.de' },
+    { urlImage: 'https://comicvine1.cbsistatic.com/uploads/scale_small/3/31666/4956367-invim2015001-maleevvar-d8a38.jpg', id: 4, name: 'Iron Man', email: 'ironman@super.de' },
+    { urlImage: 'https://comicvine1.cbsistatic.com/uploads/scale_small/14/140903/4127149-11.jpg', id: 5, name: 'Hulk', email: 'hulk@super.de' },
+    { urlImage: 'https://comicvine1.cbsistatic.com/uploads/scale_small/12/124259/7025229-rco022_1564570505.jpg', id: 6, name: 'Captain America', email: 'captainameric@super.de' },
+];
